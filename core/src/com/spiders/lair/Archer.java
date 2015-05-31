5d3a09911c6fe8b2076b4d6707601123dd7890b1
ATalaba <a.a.talaba@gmail.com>
2015-05-30 23:24:02 -0400

    Added enemy skeleton classes

---
+++ core/src/com/spiders/lair/Archer.java
@@ -0,0 +1,29 @@
+package com.spiders.lair;
+
+public class Archer implements Enemy {
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void shoot() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void loseHealth() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getHealth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ core/src/com/spiders/lair/SpinningSentry.java
@@ -0,0 +1,29 @@
+package com.spiders.lair;
+
+public class SpinningSentry implements Enemy {
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void shoot() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void loseHealth() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getHealth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
