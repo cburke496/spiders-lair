1ce8022c58705065912dd2f7f4c1654ecf5b4e2b
ATalaba <a.a.talaba@gmail.com>
2015-05-31 23:17:43 -0400

    Changed random map generation: Rooms have door in a path to center but can randomly move away from center

--- core/src/com/spiders/lair/Map.java
+++ core/src/com/spiders/lair/Map.java
@@ -9,29 +9,32 @@ import java.util.HashMap;
 public class Map {
 	
 	//The chance that a newly-created room will be connected to the previous room
-	private static final float connectionChance = 0.5f;
+	private static final float ENLARGEN_CHANCE = 0.3f;
+	
+	//The chance that the path will go away from the center
+	private static final float STRAY_CHANCE = 0.2f;
 	
 	//Colors given as RGB values between 0 and 1
-	private static final float[] wallColor = {0.35f, 0.35f, 0.35f};
-	private static final float[] floorColor = {0.6f, 0.85f, 0.92f};
+	private static final float[] WALL_COLOR = {0.35f, 0.35f, 0.35f};
+	private static final float[] FLOOR_COLOR = {0.6f, 0.85f, 0.92f};
 	
 	//The size of any wall given as a percentage of the height of the whole room
-	private static final float wallSize = 0.2f;
+	private static final float WALL_SIZE = 0.2f;
 	
 	//The size of any doorway given as a percentage of the height of the whole room
-	private static final float doorSize = 0.3f;
+	private static final float DOOR_SIZE = 0.3f;
 	
 	private Array<Array<Room>> data;
 	private Room currentRoom;
 	private int cx, cy;
 	private int numRooms;
-	private HashMap<Room, Array<Room>> roomConnections;
+	private HashMap<Room, Array<Room>> largeRooms;
 	
 	public Map(int width, int height) {
 		cx = width/2;
 		cy = height/2;
 		numRooms = 0;
-		roomConnections = new HashMap<Room, Array<Room>>();
+		largeRooms = new HashMap<Room, Array<Room>>();
 		
 		data = new Array<Array<Room>>(height);
 		for(int i = 0; i < height; i++) {
@@ -65,11 +68,35 @@ public class Map {
 		//to the center, until they finally reach the center coordinates
 		while(x != cx || y != cy) {
 			int newX = x, newY = y;
-			if(x > cx) newX = x - 1;
-			if(x < cx) newX = x + 1;
-			if(y > cy) newY = y - 1;
-			if(y < cy) newY = y + 1;
-			
+			if(x > cx) {
+				if (Math.random() > STRAY_CHANCE || x == 9) {
+					newX = x - 1;
+				} else {
+					newX = x + 1;
+				}
+			}
+			if(x < cx) {
+				if (Math.random() > STRAY_CHANCE || x == 0) {
+					newX = x + 1;
+				} else {
+					newX = x - 1;
+				}
+			}
+			if(y > cy) {
+				if (Math.random() > STRAY_CHANCE || y == 9) {
+					newY = y - 1;
+				} else {
+					newY = y + 1;
+				}
+			}
+			if(y < cy) {
+				if (Math.random() > STRAY_CHANCE || y == 0) {
+					newY = y + 1;
+				} else {
+					newY = y - 1;
+				}
+			}
+
 			//newRoom stores the next room in the chain after lastChecked
 			Room newRoom = null;
 			
@@ -84,9 +111,12 @@ public class Map {
 						
 						//There is a chance that the newly-created room will be
 						//connected to the previous room
-						if (Math.random() < connectionChance)
-							addConnection(lastChecked, newRoom);
-						
+						if (Math.random() < ENLARGEN_CHANCE) {
+							enlargen(lastChecked, newRoom);
+						} else {
+							lastChecked.addConnection(newRoom);
+							newRoom.addConnection(lastChecked);
+						}
 						lastChecked = newRoom;
 						numRooms++;
 					}
@@ -96,7 +126,10 @@ public class Map {
 						data.get(newY).set(x, newRoom);
 						
 						if (Math.random() < 0.5) {
-							addConnection(lastChecked, newRoom);
+							enlargen(lastChecked, newRoom);
+						} else {
+							lastChecked.addConnection(newRoom);
+							newRoom.addConnection(lastChecked);
 						}
 						
 						lastChecked = newRoom;
@@ -117,10 +150,15 @@ public class Map {
 					//possible for both (newX,y) and (x,newY) to be null
 					assert hRoom != null || vRoom != null;
 					
-					if(Math.random() < 0.5)
+					if(Math.random() < 0.5) {
 						lastChecked = hRoom == null ? vRoom : hRoom;
-					else
-						lastChecked = vRoom == null ? hRoom : vRoom;
+						data.get(y).get(x).addConnection(lastChecked);
+						lastChecked.addConnection(data.get(y).get(x));
+						
+					}
+					else {
+						lastChecked = vRoom == null ? hRoom : vRoom; 
+					}
 				}
 			}
 			
@@ -134,32 +172,40 @@ public class Map {
 				newRoom = new Room(x,y);
 				data.get(y).set(x, newRoom);
 				
-				if(Math.random() < connectionChance)
-					addConnection(lastChecked, newRoom);
-					
+				if (Math.random() < ENLARGEN_CHANCE) {
+					enlargen(lastChecked, newRoom);
+				} else {
+					lastChecked.addConnection(newRoom);
+					newRoom.addConnection(lastChecked);
+				}
 				numRooms++;
+			} else {
+				lastChecked.addConnection(newRoom);
+				newRoom.addConnection(lastChecked);
 			}
 			
 			//lastChecked is updated to the room at (newX,newY)
 			lastChecked = newRoom;
 		}
-			
+		
+		
+					
 	}
 
-	private void addConnection(Room lastChecked, Room newRoom) {
-		Array<Room> oldArray = roomConnections.get(lastChecked);
+	private void enlargen(Room lastChecked, Room newRoom) {
+		Array<Room> oldArray = largeRooms.get(lastChecked);
 		if(oldArray == null) {
 			oldArray = new Array<Room>();
 		}
 		oldArray.add(newRoom);
-		roomConnections.put(lastChecked, oldArray);
+		largeRooms.put(lastChecked, oldArray);
 		
-		Array<Room> newArray = roomConnections.get(newRoom);
+		Array<Room> newArray = largeRooms.get(newRoom);
 		if(newArray == null) {
 			newArray = new Array<Room>();
 		}
 		newArray.add(lastChecked);
-		roomConnections.put(newRoom, newArray);
+		largeRooms.put(newRoom, newArray);
 	}
 	
 	public void drawMap(ShapeRenderer sr) {
@@ -167,22 +213,22 @@ public class Map {
 		int width = data.get(0).size;
 		int roomW = SpidersLair.width / width;
 		int roomH = SpidersLair.height / height;
-		int wallOffset = (int) (wallSize / 2 * roomH);
+		int wallOffset = (int) (WALL_SIZE / 2 * roomH);
 		//For all existing rooms, this draws the walls and then the floors
 		for(int y = 0; y < height; y++) {
 			for(int x = 0; x < width; x++) {
 				if(data.get(y).get(x) != null) {
 					sr.begin(ShapeType.Filled);
-					sr.setColor(wallColor[0],wallColor[1],wallColor[2],1f);
+					sr.setColor(WALL_COLOR[0],WALL_COLOR[1],WALL_COLOR[2],1f);
 					sr.rect(x*roomW-wallOffset,y*roomH-wallOffset,roomW+2*wallOffset,roomH+2*wallOffset);
-					sr.setColor(floorColor[0],floorColor[1],floorColor[2],1f);
+					sr.setColor(FLOOR_COLOR[0],FLOOR_COLOR[1],FLOOR_COLOR[2],1f);
 					sr.rect(x*roomW+wallOffset,y*roomH+wallOffset,roomW-2*wallOffset,roomH-2*wallOffset);
 					sr.end();
 				}
 			}
 		}
 		
-		int doorOffset = (int) (doorSize / 2 * roomH);
+		int doorOffset = (int) (DOOR_SIZE / 2 * roomH);
 		
 		//For all rooms that are horizontally adjacent, this checks whether or not
 		//they should be connected into one room, and then either connects them
@@ -194,12 +240,12 @@ public class Map {
 					Room right = data.get(y).get(x+1);
 					if(right != null) {
 						sr.begin(ShapeType.Filled);
-						sr.setColor(floorColor[0],floorColor[1],floorColor[2],1f);
+						sr.setColor(FLOOR_COLOR[0],FLOOR_COLOR[1],FLOOR_COLOR[2],1f);
 						
-						Array<Room> connections = roomConnections.get(current);
-						if(connections != null && connections.contains(right, true))
+						Array<Room> roomSize = largeRooms.get(current);
+						if(roomSize != null && roomSize.contains(right, true))
 							sr.rect((x+1)*roomW-wallOffset,y*roomH+wallOffset,2*wallOffset,roomH-2*wallOffset);
-						else
+						else if(current.connectedTo(right))
 							sr.rect((x+1)*roomW-wallOffset,(y+0.5f)*roomH-doorOffset,2*wallOffset,2*doorOffset);
 						sr.end();
 					}
@@ -214,12 +260,12 @@ public class Map {
 					Room up = data.get(y+1).get(x);
 					if(up != null) {
 						sr.begin(ShapeType.Filled);
-						sr.setColor(floorColor[0],floorColor[1],floorColor[2],1f);
+						sr.setColor(FLOOR_COLOR[0],FLOOR_COLOR[1],FLOOR_COLOR[2],1f);
 						
-						Array<Room> connections = roomConnections.get(current);
-						if(connections != null && connections.contains(up,true))
+						Array<Room> roomSize = largeRooms.get(current);
+						if(roomSize != null && roomSize.contains(up,true))
 							sr.rect(x*roomW+wallOffset,(y+1)*roomH-wallOffset,roomW-2*wallOffset,wallOffset*2);
-						else
+						else if(current.connectedTo(up))
 							sr.rect((x+0.5f)*roomW-doorOffset,(y+1)*roomH-wallOffset,2*doorOffset,2*wallOffset);
 						sr.end();
 					}
@@ -244,13 +290,13 @@ public class Map {
 	}
 	
 	
-	public static void main(String[] args) {
-		Map tester = new Map(10, 10);
-//		for(int i = 0; i < 1; i++) {
-//			System.out.println(new Map(10, 10).toString());
-//		}
-		System.out.println(tester.toString());
-		System.out.println(tester.roomConnections.toString());
-	}
+//	public static void main(String[] args) {
+//		Map tester = new Map(10, 10);
+////		for(int i = 0; i < 1; i++) {
+////			System.out.println(new Map(10, 10).toString());
+////		}
+//		System.out.println(tester.toString());
+//		System.out.println(tester.largeRooms.toString());
+//	}
 	
 }
--- core/src/com/spiders/lair/Room.java
+++ core/src/com/spiders/lair/Room.java
@@ -1,12 +1,16 @@
 package com.spiders.lair;
 
+import com.badlogic.gdx.utils.Array;
+
 public class Room {
 
 	private int x, y;
+	private Array<Room> connections;
 	
 	public Room(int x, int y) {
 		this.x = x;
 		this.y = y;
+		connections = new Array<Room>();
 	}
 	
 	public int mapx() {
@@ -19,5 +23,15 @@ public class Room {
 	
 	public String toString() {
 		return "("+x+","+y+")";
+	}
+	
+	public void addConnection(Room newRoom) {
+		if (!connections.contains(newRoom, true)) {
+			connections.add(newRoom);
+		}
+	}
+	
+	public boolean connectedTo(Room newRoom) {
+		return connections.contains(newRoom, true);
 	}
 }
